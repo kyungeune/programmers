356b93a12b8906836685bfea0d696a0d01f10620
kyungeune <132311633+kyungeune@users.noreply.github.com>
2026-01-19 16:17:09 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -4,10 +4,11 @@ def solution(arr):
     for i in range(len(arr) - 1):
         b = arr[i+1]
         
-        x, y = a, b  # 최소공배수 구하기
+        x, y = a, b  # 최대공약수 : x
         while y != 0:
             x, y = y, x % y
         
+        # 두 수의 곱 / 최대공약수 = 최소공배수
         a = a * b // x
     
     return a
